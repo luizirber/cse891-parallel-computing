de1af8a7be23598dddc4a2f608c87ae0c3792825
Luiz Irber <irberlui@msu.edu>
2014-10-07 17:46:41 -0400

    Renamed N to VECTOR_SIZE and add proper macro

--- homeworks/homework2/src/alg2.c
+++ homeworks/homework2/src/alg2.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define N 100
+#ifndef VECTOR_SIZE
+#define VECTOR_SIZE 100
+#endif
+
 #define SUM_TAG 2
 #define DISTRIBUTE_TAG 3
 
@@ -17,7 +20,7 @@ int calc_chunk_for_proc(int proc, int problem_size, int procs) {
 
 
 int init_vector(int vect[]) {
-    for (int i = 0; i < N; ++i ) {
+    for (int i = 0; i < VECTOR_SIZE; ++i ) {
       vect[i] = rand() % 100;
     }
     return 0;
@@ -27,8 +30,8 @@ int init_vector(int vect[]) {
 int distribute_vector(int vect[], int procs) {
     int chunk_size = 0;
     for (int i = 1; i < procs; ++i ) {
-      chunk_size = calc_chunk_for_proc(i, N, procs);
-      MPI_Send(vect + i * (N / procs), chunk_size, MPI_INT, i, DISTRIBUTE_TAG, MPI_COMM_WORLD);
+      chunk_size = calc_chunk_for_proc(i, VECTOR_SIZE, procs);
+      MPI_Send(vect + i * (VECTOR_SIZE / procs), chunk_size, MPI_INT, i, DISTRIBUTE_TAG, MPI_COMM_WORLD);
     }
     return 0;
 }
@@ -36,7 +39,7 @@ int distribute_vector(int vect[], int procs) {
 
 int receive_vector(int vect[], int procs, int rank) {
     MPI_Status status;
-    int chunk_size = calc_chunk_for_proc(rank, N, procs);
+    int chunk_size = calc_chunk_for_proc(rank, VECTOR_SIZE, procs);
     MPI_Recv(vect, chunk_size, MPI_INT, 0, DISTRIBUTE_TAG, MPI_COMM_WORLD, &status);
     return 0;
 }
@@ -56,7 +59,7 @@ int main(int argc, char **argv)
    int rank, procs;
    long long int my_sum = 0;
    long long int other_sum = 0;
-   int vector[N];
+   int vector[VECTOR_SIZE];
    int chunk_size;
 
    MPI_Status status;
@@ -64,7 +67,7 @@ int main(int argc, char **argv)
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &procs);
 
-   chunk_size = calc_chunk_for_proc(rank, N, procs);
+   chunk_size = calc_chunk_for_proc(rank, VECTOR_SIZE, procs);
 
    if (rank == 0) {
       init_vector(vector);
@@ -79,7 +82,7 @@ int main(int argc, char **argv)
 
       /*
       long long int serial_sum = 0;
-      for (int i = 0; i < N; ++i)
+      for (int i = 0; i < VECTOR_SIZE; ++i)
         serial_sum += vector[i];
 
       if (serial_sum == my_sum) {
