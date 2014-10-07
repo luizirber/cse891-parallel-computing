e56f9d6fd308aacb5b4696a7c41bb2118a22ce6b
Luiz Irber <irberlui@msu.edu>
2014-10-07 17:03:03 -0400

    Add TAU to runtime, create functions on alg2 to make it easier to check timing

--- homeworks/homework2/src/alg2.c
+++ homeworks/homework2/src/alg2.c
@@ -7,6 +7,15 @@
 #define DISTRIBUTE_TAG 3
 
 
+int calc_chunk_for_proc(int proc, int problem_size, int procs) {
+  int chunk_size = problem_size / procs;
+  if (proc == procs - 1) {
+    chunk_size = problem_size - proc * chunk_size;
+  }
+  return chunk_size;
+}
+
+
 int init_vector(int vect[]) {
     for (int i = 0; i < N; ++i ) {
       vect[i] = rand() % 100;
@@ -18,8 +27,8 @@ int init_vector(int vect[]) {
 int distribute_vector(int vect[], int procs) {
     int chunk_size = 0;
     for (int i = 1; i < procs; ++i ) {
-      chunk_size = N / procs;
-      MPI_Send(vect + i * chunk_size, chunk_size, MPI_INT, i, DISTRIBUTE_TAG, MPI_COMM_WORLD);
+      chunk_size = calc_chunk_for_proc(i, N, procs);
+      MPI_Send(vect + i * (N / procs), chunk_size, MPI_INT, i, DISTRIBUTE_TAG, MPI_COMM_WORLD);
     }
     return 0;
 }
@@ -27,8 +36,18 @@ int distribute_vector(int vect[], int procs) {
 
 int receive_vector(int vect[], int procs, int rank) {
     MPI_Status status;
-    MPI_Recv(vect, N / procs, MPI_INT, 0, DISTRIBUTE_TAG, MPI_COMM_WORLD, &status);
+    int chunk_size = calc_chunk_for_proc(rank, N, procs);
+    MPI_Recv(vect, chunk_size, MPI_INT, 0, DISTRIBUTE_TAG, MPI_COMM_WORLD, &status);
     return 0;
+}
+
+
+int sum_vector(int vector[], int chunk_size) {
+  long long int my_sum = 0;
+  for (int i = 0; i < chunk_size; ++i ) {
+    my_sum += vector[i];
+  }
+  return my_sum;
 }
 
 
@@ -38,47 +57,42 @@ int main(int argc, char **argv)
    long long int my_sum = 0;
    long long int other_sum = 0;
    int vector[N];
+   int chunk_size;
 
    MPI_Status status;
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &procs);
 
+   chunk_size = calc_chunk_for_proc(rank, N, procs);
+
    if (rank == 0) {
       init_vector(vector);
       distribute_vector(vector, procs);
 
-      my_sum = 0;
-      for (int i = 0; i < N / procs; ++i ) {
-         my_sum += vector[i];
-      }
-      printf("Rank 0 sum: %d\n", my_sum);
+      my_sum = sum_vector(vector, chunk_size);
 
       for (int i = 1; i < procs; ++i ) {
          MPI_Recv(&other_sum, 1, MPI_LONG_LONG_INT, i, SUM_TAG, MPI_COMM_WORLD, &status);
          my_sum += other_sum;
-         printf("Rank %d sum: %d | total: %d\n", i, other_sum, my_sum);
       }
 
-      printf("Sum value: %d\n", my_sum);
-
+      /*
       long long int serial_sum = 0;
       for (int i = 0; i < N; ++i)
         serial_sum += vector[i];
 
       if (serial_sum == my_sum) {
-        printf("Serial == parallel\n");
+        printf("DEBUG: Serial == parallel\n");
       } else {
-        printf("Serial != parallel\n");
+        printf("DEBUG: Serial != parallel, %d\n", abs(serial_sum - my_sum));
       }
+      */
 
    } else {
-      my_sum = 0;
       receive_vector(vector, procs, rank);
 
-      for (int i = 0; i < N / procs; ++i ) {
-         my_sum += vector[i];
-      }
+      my_sum = sum_vector(vector, chunk_size);
 
       MPI_Send(&my_sum, 1, MPI_LONG_LONG_INT, 0, SUM_TAG, MPI_COMM_WORLD);
    }
